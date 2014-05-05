f7b13b4fb8ecec4e08fd465144aebcc42605c995
Matt Hayes <matt@mysterycommand.com>
2014-05-04 19:54:38 -0700

    Add tests for files, and expected input.

--- test/test-creation.js
+++ test/test-creation.js
@@ -21,13 +21,54 @@ describe('bivy generator', function () {
 
     it('creates expected files', function (done) {
         var expected = [
-            // add files you expect to exist here.
+            // configs
+            '.editorconfig',
+            '.gitignore',
             '.jshintrc',
-            '.editorconfig'
+
+            // projectFiles
+            'bower.json',
+            'Gruntfile.js',
+            'package.json',
+            'README.md',
+
+            // app
+            'app/.htaccess',
+            'app/404.html',
+            'app/index.html',
+            'app/robots.txt',
+            'app/scripts/config.js',
+            'app/scripts/main.js',
+            'app/styles/_variables.scss',
+            'app/styles/main.scss',
+
+            // tasks
+            'tasks/clean.js',
+            'tasks/htmlmin.js',
+            'tasks/mocha.js',
+            'tasks/rev.js',
+            'tasks/usemin.js',
+            'tasks/watch.js',
+            'tasks/connect.js',
+            'tasks/imagemin.js',
+            'tasks/modernizr.js',
+            'tasks/sass.js',
+            'tasks/useminPrepare.js',
+            'tasks/copy.js',
+            'tasks/jshint.js',
+            'tasks/requirejs.js',
+            'tasks/svgmin.js',
+            'tasks/usereplace.js',
+
+            // test
+            'test/index.html',
+            'test/spec/config.js',
+            'test/spec/main.js'
         ];
 
         helpers.mockPrompt(this.app, {
-            'someOption': true
+            'githubUser': 'mysterycommand',
+            'appName': 'Bivy'
         });
 
         this.app.options['skip-install'] = true;
